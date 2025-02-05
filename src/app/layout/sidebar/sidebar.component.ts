9dcf9583e11d81fcfda9bf70c2ab0ee75b524231
Gabriel Calixto <gfcdev@outlook.com.br>
2025-02-05 16:22:21 -0300

    renomeado componente de sidenav para sidebar; removidas importações desnecessárias no componente sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,5 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-import { AvatarModule } from 'primeng/avatar';
-import { StyleClassModule } from 'primeng/styleclass';
-import { Sidebar } from 'primeng/sidebar';
+
 
 @Component({
   selector: 'app-sidebar',
